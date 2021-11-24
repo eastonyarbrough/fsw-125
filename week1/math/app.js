9eaba9094d8a33f6626499aeb8a2424a3e9c8fa7
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-11-23 22:42:15 -0600

    Complete 'Node Module Basics and Patterns' assignment

---
+++ week1/math/app.js
@@ -0,0 +1,8 @@
+const mathFuncs = require('./math.js');
+
+console.log(
+    mathFuncs.addition(5, 5),
+    mathFuncs.subtraction(10, 2),
+    mathFuncs.multiplication(10, 5),
+    mathFuncs.division(10, 4)
+);
